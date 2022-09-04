70672f94155f7f2e529506110bcddbc5d18de49e
Fernando Salamanca <lfernandosalamanca@gmail.com>
2022-09-04 23:06:05 +0200

    added router layout for persistent layout components

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/home';
 import { LoginPage } from './pages/login';
+import { RouterLayout } from './RouterLayout';
 
 export const AppRouter: React.FC = () => {
     return (
         <Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<HomePage />} />
+            <Route element={<RouterLayout />}>
+                <Route path="/" element={<HomePage />} />
+            </Route>
         </Routes>
     );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../../components/ui/Header';
 
 export const HomePage: React.FC = () => {
     return (
         <div>
-            <Header />
             <h1 className="headline">You are at Home!</h1>
         </div>
     );
